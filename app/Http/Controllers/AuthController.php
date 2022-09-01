980d80ce05a9ee92967572efd8218b4dffccfecb
Daniel Araneda <daniel.araneda.h@gmail.com>
2022-09-01 00:34:00 -0400

    Se deja 100% operativo

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+//use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+//use Illuminate\Foundation\Bus\DispatchesJobs;
+//use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
-use Illuminate\Routing\Controller as BaseController;
+//use Illuminate\Routing\Controller as BaseController;
 
 class AuthController extends Controller
 {
